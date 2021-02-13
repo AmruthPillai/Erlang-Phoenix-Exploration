fbbabf8b0d4a484b39b1fd3fd457e6f025d1c17d
AmruthPillai <im.amruth@gmail.com>
2021-02-13 15:16:28 +0530

    - writing more unit tests

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -3,6 +3,17 @@ defmodule CardsTest do
   doctest Cards
 
   test "greets the world" do
-    assert Cards.hello() == :world
+    assert(Cards.hello() == :world)
   end
+
+  test "create_deck returns 52 cards" do
+    deck_length = length(Cards.create_deck())
+    assert(deck_length == 52)
+  end
+
+  test "shuffling a deck works as expected" do
+    deck = Cards.create_deck()
+    refute(deck == Cards.shuffle(deck))
+  end
+
 end
