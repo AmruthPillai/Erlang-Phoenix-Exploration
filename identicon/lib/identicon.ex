259653c1f978b8c19858d026c9da3e35a5674d1a
AmruthPillai <im.amruth@gmail.com>
2021-02-13 22:50:30 +0530

    - mirroring a row

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,9 +3,20 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
-  def pick_color(%Identicon.Image{hex: [r, g, b, _tail]} = image) do
+  def build_grid(%Identicon.Image{hex: hex} = _image) do
+    hex
+    |> Enum.chunk_every(3, 3, :discard)
+    |> mirror_row
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
